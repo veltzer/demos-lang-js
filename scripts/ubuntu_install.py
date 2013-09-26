36c5df3b074d989e5e4ec7e659325295972c0480
Mark Veltzer <mark.veltzer@gmail.com>
2013-09-26 15:28:55 +0300

    fixed up extjs examples

--- scripts/ubuntu_install.py
+++ scripts/ubuntu_install.py
@@ -7,6 +7,7 @@ import subprocess # for check_call
 import urllib2 # for urlopen
 import os # for mkdir
 import os.path # for isdir, isfile
+import zipfile # for ZipFile
 
 def download(url,output_file):
 	if not os.path.isfile(output_file):
@@ -39,8 +40,27 @@ subprocess.check_call(args)
 
 # now install stuff which is not in the ubuntu store...
 
+# this is so apache will be able to access all the stuff that we create below...
+os.umask(0022)
+
 if not os.path.isdir('download'):
 	os.mkdir('download')
 
 download('http://code.highcharts.com/zips/Highcharts-3.0.5.zip', 'download/Highcharts-3.0.5.zip')
-download('http://cdn.sencha.com/ext/gpl/ext-4.2.1-gpl.zip', 'download/ext-4.2.1-gpl.zip') 
+if not os.path.isdir('download/Highcharts'):
+	os.mkdir('download/Highcharts')
+	z=zipfile.ZipFile('download/Highcharts-3.0.5.zip', 'r')
+	z.extractall('download/Highcharts')
+	z.close()
+
+folder='download/extjs'
+fname='ext-4.2.1-gpl.zip'
+url_prefix='http://cdn.sencha.com/ext/gpl'
+local_file=os.path.join('download', fname)
+remote_file=os.path.join(url_prefix, fname)
+download(remote_file, local_file) 
+if not os.path.isdir(folder):
+	z=zipfile.ZipFile(local_file, 'r')
+	z.extractall('download')
+	z.close()
+	os.rename('download/ext-4.2.1.883', folder)
