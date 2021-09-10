e61bec97604dfd975302f7a0d0e79ecf6420408f
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-10 19:19:40 +0300

    fix config

--- config/project.py
+++ config/project.py
@@ -1,17 +1,17 @@
 import config.general
 
-project_github_username = 'veltzer'
-project_name = 'demos-javascript'
+project_github_username = "veltzer"
+project_name = "demos-javascript"
 github_repo_name = project_name
-project_website = 'https://{project_github_username}.github.io/{project_name}'.format(**locals())
-project_website_source = 'https://github.com/{project_github_username}/{project_name}'.format(**locals())
-project_website_git = 'git://github.com/{project_github_username}/{project_name}.git'.format(**locals())
-# project_paypal_donate_button_id='ASPRXR59H2NTQ'
-# project_google_analytics_tracking_id='UA-56436979-1'
-project_long_description = 'Demos for the JavaScript programming language'
+project_website = f"https://{project_github_username}.github.io/{project_name}"
+project_website_source = f"https://github.com/{project_github_username}/{project_name}"
+project_website_git = f"git://github.com/{project_github_username}/{project_name}.git"
+# project_paypal_donate_button_id="ASPRXR59H2NTQ"
+# project_google_analytics_tracking_id="UA-56436979-1"
+project_long_description = "Demos for the JavaScript programming language"
 project_short_description = project_long_description
 # keywords to put on html pages or for search, dont put the name of the project or my details
 # as they will be added automatically...
-project_license = 'GPL3'
-project_year_started = '2013'
+project_license = "GPL3"
+project_year_started = "2013"
 project_description = project_long_description
