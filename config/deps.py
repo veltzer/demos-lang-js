84d7db2a67ce5ea46967a38a9c21a8cb7df1a8f6
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-14 19:00:15 +0300

    deps change

--- config/deps.py
+++ config/deps.py
@@ -1,37 +1,37 @@
 packages=[
     # libraries
-    'libjs-jquery',
-    'libjs-jquery-mobile',
-    'libjs-prototype',
-    'libjs-json',
-    'libjs-raphael',
-    'libjs-jquery-ui',
-    'libjs-jquery-ui-docs',
-    'libjs-jquery-ui-theme-ui-lightness',
-    'libjs-dojo-core',
-    'libjs-dojo-dijit',
-    'libjs-dojo-dojox',
+    "libjs-jquery",
+    "libjs-jquery-mobile",
+    "libjs-prototype",
+    "libjs-json",
+    "libjs-raphael",
+    "libjs-jquery-ui",
+    "libjs-jquery-ui-docs",
+    "libjs-jquery-ui-theme-ui-lightness",
+    "libjs-dojo-core",
+    "libjs-dojo-dijit",
+    "libjs-dojo-dojox",
 
     # extjs is no longer a part of ubuntu
-    #'libjs-extjs',
-    #'libjs-extjs-doc',
+    #"libjs-extjs",
+    #"libjs-extjs-doc",
 
     # yui3 is no longer a part of ubuntu
-    # 'libjs-yui3-common',
-    # 'libjs-yui3-debug',
-    # 'libjs-yui3-doc',
-    # 'libjs-yui3-full',
-    # 'libjs-yui3-min',
+    # "libjs-yui3-common",
+    # "libjs-yui3-debug",
+    # "libjs-yui3-doc",
+    # "libjs-yui3-full",
+    # "libjs-yui3-min",
 
-    'libjs-backbone',
-    'libjs-mootools',
-    'libjs-scriptaculous',
+    "libjs-backbone",
+    "libjs-mootools",
+    "libjs-scriptaculous",
     # tools
-    'yui-compressor',
-    'slimit',
-    'shrinksafe',
-    #'yui-builder',
+    "yui-compressor",
+    "slimit",
+    "shrinksafe",
+    #"yui-builder",
 ]
 tools=[
-    'jsl',
+    "jsl",
 ]
