be0e3b670dfa10b0acbc1ad822725d24d1087108
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 13:16:17 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,9 +1,11 @@
 """ python deps for this project """
 
-build_requires: list[str] = [
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
+
+import config.shared
+
+install_requires: list[str] = [
     "pyrelist",
 ]
-requires = build_requires
+build_requires: list[str] = config.shared.BUILD
+test_requires: list[str] = config.shared.TEST
+requires = install_requires + build_requires + test_requires
